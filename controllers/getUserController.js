c9020914026441c4a21029a243170809b1944460
Evgeny Karpel <kspshnik@yandex.ru>
2021-04-29 04:33:45 +0300

    feature: getUserController

---
+++ controllers/getUserController.js
@@ -0,0 +1,21 @@
+const UserModel = require('../models/UserModel');
+
+const NotFoundError = require('../errors/NotFoundError');
+
+async function getUserController(req, res, next) {
+  const id = req.params;
+  try {
+    const user = await UserModel.findById(id);
+
+    if (user) {
+      const { name, email } = user;
+      res.status(200).json({ name, email });
+    } else {
+      throw new NotFoundError(`Пользователь с идентификатором ${id} не найден`);
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = getUserController;
