f930cc1f46bd02d3aaedb0b87abf70db1dd6cc70
Evgeny Karpel <kspshnik@yandex.ru>
2021-04-29 06:22:24 +0300

    fix: путь слушания

--- movies-server/routers/signInRouter.js
+++ movies-server/routers/signInRouter.js
@@ -3,6 +3,6 @@ const signInRouter = require('express').Router();
 const signInController = require('../controllers/signInController');
 const celebrateSignIn = require('../middlewares/celebrateSignIn');
 
-signInRouter.post('/signin', celebrateSignIn, signInController);
+signInRouter.post('/', celebrateSignIn, signInController);
 
 module.exports = signInRouter;
--- movies-server/routers/signUpRouter.js
+++ movies-server/routers/signUpRouter.js
@@ -3,6 +3,6 @@ const signUpRouter = require('express').Router();
 const signUpController = require('../controllers/signUpController');
 const celebrateSignUp = require('../middlewares/celebrateSignUp');
 
-signUpRouter.post('/signup', celebrateSignUp, signUpController);
+signUpRouter.post('/', celebrateSignUp, signUpController);
 
 module.exports = signUpRouter;
