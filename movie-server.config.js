acb8c90b9290ec3546cf9d5a16c8793fd1f1eac5
Evgeny Karpel <kspshnik@yandex.ru>
2021-04-29 04:22:04 +0300

    feature: signInRouter

--- movie-server.config.js
+++ movie-server.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const {
-  PORT = 5000,
+  PORT = 3000,
   DB_ADDRESS = 'mongodb://localhost:27017/moviesdb',
   JWT_KEY = 'fe0efc9c0c561d176ba8908df811811b7a061b851e8c9c87fb5609f21268f978',
   NODE_ENV,
