4d6d878b471793aded4b0fa5ef34b89d2b0c8f72
Evgeny Karpel <kspshnik@yandex.ru>
2021-04-30 22:45:45 +0300

    ESLint --fix

--- movies-server/movie-server.config.js
+++ movies-server/movie-server.config.js
@@ -8,4 +8,6 @@ const {
   JWT_KEY = 'fe0efc9c0c561d176ba8908df811811b7a061b851e8c9c87fb5609f21268f978',
   NODE_ENV,
 } = process.env;
-module.exports = { PORT, DB_ADDRESS, JWT_KEY, NODE_ENV };
+module.exports = {
+  PORT, DB_ADDRESS, JWT_KEY, NODE_ENV,
+};
