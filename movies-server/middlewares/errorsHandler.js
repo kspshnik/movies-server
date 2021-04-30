8cf2881e0bf5c95d4fbb70af33135c76053b303f
Evgeny Karpel <kspshnik@yandex.ru>
2021-04-30 22:44:35 +0300

    refactor: обработка Е11000 в централизованном обработчике

--- movies-server/middlewares/errorsHandler.js
+++ movies-server/middlewares/errorsHandler.js
@@ -1,6 +1,8 @@
 function errorsHandler(err, req, res, next) {
   const { statusCode = 500, message } = err;
-  if (err && err.message && err.message.includes('celebrate')) {
+  if (err.code && err.code === 11000 && err.keyValue && err.keyValue.email) {
+    res.status(409).json({ message: `Пользователь с адресом электронной почты ${err.keyValue.email} уже существует` });
+  } else if (err && err.message && err.message.includes('celebrate')) {
     let msg;
     if (err.details.get('body')) {
       msg = err.details.get('body').message;
