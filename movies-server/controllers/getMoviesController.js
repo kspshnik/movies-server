900cc81fbc75e51748b756a64cd397d3b777496f
Evgeny Karpel <kspshnik@yandex.ru>
2021-06-05 18:02:00 +0300

    fix: отбор по пользователю

--- movies-server/controllers/getMoviesController.js
+++ movies-server/controllers/getMoviesController.js
@@ -1,8 +1,9 @@
 const MovieModel = require('../models/MovieModel');
 
 async function getMoviesController(req, res, next) {
+  const user = req.usr.id;
   try {
-    const movies = await MovieModel.find({});
+    const movies = await MovieModel.find({ owner: user });
     res.status(200).json(movies);
   } catch (err) {
     next(err);
